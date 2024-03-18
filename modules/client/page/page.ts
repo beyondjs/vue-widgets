536a908e758829bdb87e326ddb44fb9ab7b65ee5
Henry Box <14365005+henrybox@users.noreply.github.com>
2024-03-18 20:34:53 +0000

    feat: onQueryString method added to page controller

--- modules/client/page/page.ts
+++ modules/client/page/page.ts
@@ -1,9 +1,11 @@
+import type { URI } from '@beyond-js/kernel/routing';
 import type { PageURI } from '@beyond-js/widgets/routing';
+import type { IPageWidgetController } from '@beyond-js/widgets/controller';
 import { VueWidgetController } from '@beyond-js/vue-widgets/base';
 import { manager } from '@beyond-js/widgets/routing';
 
 export /*bundle*/
-abstract class PageVueWidgetController extends VueWidgetController {
+abstract class PageVueWidgetController extends VueWidgetController implements IPageWidgetController {
 	#uri: PageURI;
 	get uri() {
 		return this.#uri;
@@ -13,9 +15,14 @@ abstract class PageVueWidgetController extends VueWidgetController {
 		return super.mount({ uri: this.#uri });
 	}
 
+	onQueryString(qs: URI['qs']) {
+		void qs;
+	}
+
 	async initialise() {
 		const { widget } = this;
 		const uri = manager.pages.obtain({ widget });
+		uri.on('change', this.onQueryString.bind(this));
 		this.#uri = uri;
 
 		await super.initialise();
